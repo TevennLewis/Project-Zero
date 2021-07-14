4a965b6a2f4a29a8bbf7851114f51d37163065e3
TevennLewis <tevennlewis@gmail.com>
2021-07-14 14:26:00 -0700

    updated backgrounds and pictures as progressing through levels

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
 const demonSlayer = {
     name: "Tevenn Lewis",
-    time: 60,
+    time: 90,
     rank: 1,
     tCBScale: 10,
     strengthScale: 10,
@@ -19,17 +19,20 @@ let breathProgress = $("#breathScore")
  function countDownValue(){
     demonSlayer.time--;
     timer.text(`Timer: ${demonSlayer.time}sec`);
-    if(demonSlayer.tCBScale === 0 || demonSlayer.strengthScale === 0 || demonSlayer.breathTechniqueScale === 0) {
+    if(demonSlayer.tCBScale <= 0 || demonSlayer.strengthScale <= 0 || demonSlayer.breathTechniqueScale <= 0) {
         clearInterval(stopTime);
         clearInterval(stopProgress);
         $(".container").empty();
         $(".container").append($("<h1>A DEMON ATE YOU</h1>").css("font-size", "100px"));
     }
-    if(demonSlayer.time === 0 && demonSlayer.tCBScale > 0 && demonSlayer.strengthScale > 0 && demonSlayer.breathTechniqueScale > 0){
+    if(demonSlayer.time === 60 && demonSlayer.tCBScale > 0 && demonSlayer.strengthScale > 0 && demonSlayer.breathTechniqueScale > 0){
         $("#zenitsu").attr("src", "https://i.pinimg.com/originals/73/47/78/7347789b4ef2131e1de720843891bd58.jpg");
         $("body").css("background-color", "rgb(0, 204, 255)");
-        clearInterval(stopTime);
-        clearInterval(stopProgress);
+        // clearInterval(stopTime);
+        // clearInterval(stopProgress);
+    } else if (demonSlayer.time === 30 && demonSlayer.tCBScale > 0 && demonSlayer.strengthScale > 0 && demonSlayer.breathTechniqueScale > 0) {
+        $("body").css("background-color", "rgb(4, 90, 170)");
+        $("#zenitsu").attr("src", "http://images6.fanpop.com/image/photos/43400000/-Giyu-Tomioka-Demon-Slayer-anime-43458556-540-810.jpg")
     }
 
 
